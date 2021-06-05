d2d0bd1a8e459aa74a9be5d102ee795b2696f493
Camy ngo <camyngo@byu.edu>
2021-06-05 14:42:49 -0600

    display all the data

--- src/components/Pokedex.vue
+++ src/components/Pokedex.vue
@@ -1,29 +1,144 @@
 <template>
-  <div class="app">
-    <h1> HEllo there?</h1>
-  </div>
+  <div class="pokedex-background">
+    <b-container class="bv-example-row">
+    <div class="row filters">
+      <div class="col-md-4">
+        <form action="/pokedex/move-search" method="post">
+          <br/>
+          <h2 class="heading">Search By Move</h2>
+          <select multiple id='moveId' name='moveId' type='text'>
+            <option value=0 selected>All Pokemon</option>
+            #foreach($move in $moves)
+            <option value=$move.getId()>$move.getName()</option>
+            #end
+          </select>
+          <br>
+          <b-button pill type="submit" class="btn btn-danger submitSearch">
+            <span>
+              <b class="glyphicon glyphicon glyphicon-search"></b>
+              <b class="glyphicon glyphicon glyphicon-king"></b>
+              <b class="glyphicon glyphicon glyphicon-queen"></b>
+              <b class="glyphicon glyphicon glyphicon-knight"></b>
+            </span>
+          </b-button>
+        </form>
+      </div>
+
+      <div class="col-md-4 top-center">
+
+        <br/>
+        <div class="row pokedex-title">
+          <h1>The Complete Pokedex</h1>
+          <h2>Original 151</h2>
+        </div>
+
+        <div class="row">
+          <form action="/pokedex/name-search" method="post">
+            <input id="name" name="name" type="text" autocomplete="off" required>
+            <b-button pill type="submit" class="btn btn-success" id="nameSearch">Search Pok&eacute;mon By Name</b-button>
+          </form>
+        </div>
+
+        <div class="row">
+          <form action="/pokedex/strong-against-search" method="post">
+            <input id="strongAgainst" name="strongAgainst" type="text" autocomplete="off" required>
+            <b-button pill type="submit" class="btn btn-success" id="idealSearch">Enter Pok&eacute;mon For Its Ideal Foes</b-button>
+          </form>
+        </div>
+
+      </div>
+
+      <div class="col-md-4">
+        <br/>
+        <form action="/pokedex/type-search" method="post">
+          <h2 class="heading">Search By Type</h2>
+          <select multiple id='typeId' name='typeId' type='text'>
+            <option value="All" selected>All Pokemon</option>
+            <option value="Grass">Grass</option>
+            <option value="Fire">Fire</option>
+            <option value="Water">Water</option>
+            <option value="Electric">Electric</option>
+            <option value="Poison">Poison</option>
+            <option value="Bug">Bug</option>
+            <option value="Psychic">Psychic</option>
+            <option value="Ghost">Ghost</option>
+            <option value="Rock">Rock</option>
+            <option value="Ground">Ground</option>
+            <option value="Fighting">Fighting</option>
+            <option value="Normal">Normal</option>
+            <option value="Flying">Flying</option>
+            <option value="Ice">Ice</option>
+            <option value="Dragon">Dragon</option>
+          </select>
+          <br>
+          <b-button pill type="submit" class="btn btn-danger submitSearch">
+            <span>
+              <b class="glyphicon glyphicon glyphicon-search"></b>
+              <b class="glyphicon glyphicon glyphicon-leaf"></b>
+              <b class="glyphicon glyphicon glyphicon-fire"></b>
+              <b class="glyphicon glyphicon glyphicon-tint"></b>
+            </span>
+          </b-button>
+        </form>
+      </div>
+<!--      display pokedex database-->
+      <div class="container">
+          <div class="row">
+            <div class="col">
+              <section v-for="(pokemon, position) in pokemons" v-bind:key="position">
+                <b-container class="bv-example-row">
+                <div class = "col-md-3">
+                  <a href="../resources/pokemon/{pokemon.id}" class="link"/>
+                  <div class = "pokebox">
+                    <h1>#{{ pokemon.id }} </h1>
+                    <h1>{{ pokemon.name }} </h1>
+                    <img src=../resources/pokemon/Aerodactyl.gif>
+                  </div>
+                </div>
+                </b-container>
+              </section>
+            </div>
+          </div>
+      </div>
+
+    </div>
+    </b-container>
+
+
+
+    <footer class="footerDeco">
+      <br>
+      <a href="/" class="btn btn-info home">Back To Home</a>
+      <br>
+    </footer>
+    </div>
 </template>
 
 <script>
+    import {getAlldatabase} from "../json/importScript";
     export default {
-        name: 'Pokedex'
+        name: 'Pokedex',
+        data() {
+            return {
+                pokemons: []
+            }
+        },
+        async created() {
+            this.pokemons = await getAlldatabase();
+        },
+        methods: {
+            displaybyID(key){
+                return this.pokemons.filter((pokemon) => {
+                    return pokemon['id'] ==  key;
+                })
+            },
+        }
     }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  h3 {
-    margin: 40px 0 0;
-  }
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-  a {
-    color: #42b983;
+  body{
+    font-size: 2rem;
   }
 </style>
--- src/json/importScript.js
+++ src/json/importScript.js
@@ -1,9 +1,17 @@
 import pokemons from './pokemons.js';
-import firebase from './firebaseConfig';
+import firebase from '../firebaseConfig.js';
 
 const db = firebase.firestore()
-
 export async function importAllPokemons() {
     await Promise.all(pokemons.map(pokemon => db.collection('pokemon').add(pokemon)))
     console.log("who's the best? Oh yeah, Brett is")
 }
+
+export async function getAlldatabase(){
+    var pokedex = db.collection("pokemon");
+    const querySnapshot = await pokedex.get()
+    return querySnapshot.docs.map(doc => doc.data())
+}
+
+
+
