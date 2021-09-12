8bf64e941c19c95968ee306b3996e42d9122da52
Camy ngo <camyngo@byu.edu>
2021-09-11 23:24:19 -0700

    weird stuff

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,29 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../components/Home.vue'
-import Pokedex from '../components/Pokedex.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../components/Home.vue";
+import Pokedex from "../components/Pokedex.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-    {
-        path: '/',
-        name: 'home',
-        component: Home
-    },
-    {
-        path: '/pokedex',
-        name: 'deck',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: Pokedex
-    }
-]
+  {
+    path: "/",
+    name: "home",
+    component: Home,
+  },
+  {
+    path: "/pokedex",
+    name: "deck",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: Pokedex,
+  },
+];
 
 const router = new VueRouter({
-    mode: 'history',
-    base: process.env.BASE_URL,
-    routes
-})
+  base: process.env.BASE_URL,
+  routes,
+});
 
-export default router
+export default router;
